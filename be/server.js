c2ba7116883bc47851a21aa8f5b13e094c4d5051
root <root@ip-172-31-93-110.ec2.internal>
2025-11-01 18:33:27 +0000

    Test Jenkins automation - updated message

--- be/server.js
+++ be/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Docker Swarm backend!' });
+  res.json({ message: 'Hello from Updated backend!' });
 });
 app.listen(3000, () => console.log('Backend running on port 3000'));
 
