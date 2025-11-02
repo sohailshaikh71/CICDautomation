6d109bde0f6f804490ec83c4c5f9e3ab1c81211c
root <root@ip-172-31-93-110.ec2.internal>
2025-11-02 15:21:45 +0000

    Revert: Back to previous working message

--- be/server.js
+++ be/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Updated backend!' });
+  res.json({ message: 'Hello from Final Jenkins Automation Test!' });
 });
 app.listen(3000, () => console.log('Backend running on port 3000'));
 
